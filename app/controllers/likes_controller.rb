7a6c57cefb637620669a5ce5019957b9dc56887f
ryusuke-goto <ryusukeryusuke0401@gmail.com>
2024-07-16 18:58:26 +0900

    add:全体いいねアクション追加

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -11,7 +11,14 @@ class LikesController < ApplicationController
 
   def everything
     @diaries = Diary.where(diary_date: Date.today).includes(:user)
-    if @diaries
+    if @diaries.present?
+      @diaries.each do |diary|
+        puts "diaryの値は#{diary}"
+        current_user.like(diary)
+      end
+      redirect_to diaries_path, success: t('likes.everything_success')
+    else
+      redirect_to diaries_path, success: t('likes.everything_failed')
     end
   end
 end
