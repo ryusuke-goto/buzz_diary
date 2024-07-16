cb7d448708e36aabade57cdaa61747feb07107b4
ryusuke-goto <ryusukeryusuke0401@gmail.com>
2024-07-16 18:36:20 +0900

    add:全体いいねボタン追加。自分の日記にもいいねできるよう修正

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,12 +1,17 @@
 # frozen_string_literal: true
 
 class LikesController < ApplicationController
-  before_action :authenticate_user!, only: %i[create]
+  before_action :authenticate_user!
   skip_before_action :verify_authenticity_token
 
   def create
     @diary = Diary.find_by(id: params[:diary_id])
-    puts "こんにちは！#{@diary.inspect}"
-    @likes = current_user.like(@diary)
+    current_user.like(@diary)
+  end
+
+  def everything
+    @diaries = Diary.where(diary_date: Date.today).includes(:user)
+    if @diaries
+    end
   end
 end
