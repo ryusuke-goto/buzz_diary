91547d499bf9cd9e92bce2766269ffa14ced88ce
ryusuke-goto <ryusukeryusuke0401@gmail.com>
2024-07-21 15:46:11 +0900

    add:ミッションページで、ミッションの達成/未達成が表示される機能追加

--- app/controllers/missions_controller.rb
+++ app/controllers/missions_controller.rb
@@ -4,11 +4,13 @@ class MissionsController < ApplicationController
   def show
     case params[:type]
     when 'daily'
-      @missions = DailyMission.all
-      render partial: 'shared/daily_missions', locals: { daily_missions: @missions }
+      @missions = current_user.my_dailies
+      @mission_status = current_user.user_dailies.index_by(&:daily_mission_id)
+      render partial: 'shared/daily_missions', locals: { daily_missions: @missions, daily_status: @mission_status }
     when 'challenge'
-      @missions = ChallengeMission.all
-      render partial: 'shared/challenge_missions', locals: { challenge_missions: @missions }
+      @missions = current_user.my_challenges
+      @mission_status = current_user.user_challenges.index_by(&:challenge_mission_id)
+      render partial: 'shared/challenge_missions', locals: { challenge_missions: @missions, challenge_status: @mission_status }
     else
       render plain: 'Invalid mission type', status: :bad_request
     end
