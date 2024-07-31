22d718164f30fafad429e3ee43214ed71fdb549c
ryusuke-goto <ryusukeryusuke0401@gmail.com>
2024-07-31 21:37:54 +0900

    update: 画像アップロードをs3になるようcarrierwave.rbを作成や、uploaderを修正。gem fog-awsを追加

---
+++ config/initializers/carrierwave.rb
@@ -0,0 +1,13 @@
+CarrierWave.configure do |config|
+    config.fog_credentials = {
+      provider: 'AWS',
+      aws_access_key_id: ENV["AWS_ACCESS_KEY_ID"],
+      aws_secret_access_key: ENV["AWS_SECRET_ACCESS_KEY"],
+      region: 'ap-northeast-1'
+    }
+  
+    config.fog_directory  = ENV["DIRECTORY_NAME"]
+    config.cache_storage = :fog
+    config.fog_public = false
+    config.fog_attributes = { cache_control: "public, max-age=#{365.days.to_i}" } # optional, defaults to {}
+  end
