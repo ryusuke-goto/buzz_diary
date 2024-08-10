9beed1468c3891e01892ecf609054b822f75f3fe
ryusuke-goto <ryusukeryusuke0401@gmail.com>
2024-08-10 17:39:36 +0900

    update: userモデルにremindカラムを追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_07_23_142638) do
+ActiveRecord::Schema[7.1].define(version: 2024_08_10_083729) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -115,6 +115,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_07_23_142638) do
     t.string "provider"
     t.string "uid"
     t.string "name"
+    t.boolean "remind", default: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
