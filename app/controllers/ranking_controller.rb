d9487d111459bde4bc84041815ad2d81b73e41b6
ryusuke-goto <ryusukeryusuke0401@gmail.com>
2024-08-20 00:04:28 +0900

    add: rankingのindexアクション作成。ルーティング定義

--- app/controllers/ranking_controller.rb
+++ app/controllers/ranking_controller.rb
@@ -1,2 +1,7 @@
 class RankingController < ApplicationController
+  def index
+    diaries = Diary.includes(:user).with_likes_count.order(diary_date: :desc).to_a
+    max_likes_count = diaries.map(&:likes_count).max
+    @max_liked_diaries = diaries.select { |diary| diary.likes_count == max_likes_count }
+  end
 end
