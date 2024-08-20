0a4fb8701a0386184974e5af14be1b10fe589e94
ryusuke-goto <ryusukeryusuke0401@gmail.com>
2024-08-21 00:33:39 +0900

    update: rankingコントローラーのindexアクションを整理

--- app/controllers/ranking_controller.rb
+++ app/controllers/ranking_controller.rb
@@ -1,10 +1,21 @@
 class RankingController < ApplicationController
   def index
+    max_liked_diaries
+    likes_record_users
+  end
+
+  private
+
+  def max_liked_diaries
     diaries = Diary.includes(:user).with_likes_count.order(diary_date: :desc).to_a
     max_likes_count = diaries.map(&:likes_count).max
     @max_liked_diaries = diaries.select { |diary| diary.likes_count == max_likes_count }
+  end
+
+  def likes_record_users
     user_likes_record_count = Like.includes(:user).group(:user_id).count
     max_likes_record = user_likes_record_count.max
     @max_likes_record_users = User.where(id: max_likes_record).select(:name, :image)
   end
+
 end
