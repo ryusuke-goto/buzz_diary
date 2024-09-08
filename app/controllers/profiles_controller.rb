904a6aca046fea151ae9086f949d8d8756df0c41
ryusuke-goto <ryusukeryusuke0401@gmail.com>
2024-09-08 17:25:01 +0900

    update: 日記一覧ページはN+1問題になるので_avatar.html.erbを使わないよう修正。公式アカウントのQRコードを画像追加

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -2,7 +2,9 @@
 
 class ProfilesController < ApplicationController
   before_action :authenticate_user!, only: %i[show]
-  def show; end
+  def show
+    @past_count = number_of_consecutive_days
+  end
 
   def update_remind
     if current_user.toggle!(:remind)
@@ -11,5 +13,26 @@ class ProfilesController < ApplicationController
       flash.now[:danger] = t('defaults.flash_message.not_updated', item: '設定')
       render :show, status: :unprocessable_entity
     end
+  end
+
+  private
+
+  def number_of_consecutive_days
+    past_count = 0
+    my_diaries = current_user.diaries.order(created_at: :desc)
+    my_diaries.each do |diary|
+      logger.info "message::::diary: #{diary.inspect}"
+      if diary.created_at.between?((Time.zone.today - past_count.day).beginning_of_day,
+                                   (Time.zone.today - past_count.day).end_of_day)
+        past_count += 1
+      elsif diary.created_at.between?((Time.zone.today - (past_count - 1).day).beginning_of_day,
+                                      (Time.zone.today - (past_count - 1).day).end_of_day)
+        logger.info 'message::::Same created_at date as the previous diary'
+      else
+        logger.info 'message::::each loop break'
+        break
+      end
+    end
+    past_count
   end
 end
