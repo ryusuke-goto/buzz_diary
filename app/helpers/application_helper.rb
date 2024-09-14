226a306b462ae09bc9a8f12f062add7ea92dc02b
ryusuke-goto <ryusukeryusuke0401@gmail.com>
2024-09-15 01:17:12 +0900

    update: 静的OGPと動的メッセージでXシェア機能実装。ミッションページにボタンを設置

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -43,40 +43,4 @@ module ApplicationHelper
       # }
     }
   end
-
-  def assign_meta_tags(options = {})
-    defaults = t('meta_tags.defaults')
-    options.reverse_merge!(defaults)
-    site = options[:site]
-    title = options[:title]
-    description = options[:description]
-    keywords = options[:keywords]
-    image = options[:image].presence || image_url('ogp.png')
-    configs = {
-      separator: '|',
-      reverse: true,
-      site:,
-      title:,
-      description:,
-      keywords:,
-      canonical: request.original_url,
-      icon: {
-        href: image_url('placeholder.png')
-      },
-      og: {
-        type: 'website',
-        title: title.presence || site,
-        description:,
-        url: request.original_url,
-        image:,
-        site_name: site
-      },
-      twitter: {
-        site:,
-        card: 'summary_large_image',
-        image:
-      }
-    }
-    set_meta_tags(configs)
-  end
 end
