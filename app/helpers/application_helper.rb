61f21d29780768e8399012288f5df15cf5255970
ryusuke-goto <ryusukeryusuke0401@gmail.com>
2024-09-14 09:23:04 +0900

    add: 静的OGPが出るように修正

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -7,8 +7,41 @@ module ApplicationHelper
   end
 
   def show_meta_tags
-    assign_meta_tags if display_meta_tags.blank?
+    default_meta_tags if display_meta_tags.blank?
     display_meta_tags
+  end
+
+  def default_meta_tags
+    {
+      site: 'バズダイアリー',
+      title: 'チャレンジミッション達成',
+      reverse: true,
+      separator: '|',   #Webサイト名とページタイトルを区切るために使用されるテキスト
+      description: 'チャレンジミッションを達成したことを共有します',
+      keywords: 'バズダイアリー',   #キーワードを「,」区切りで設定する
+      canonical: request.original_url,   #優先するurlを指定する
+      noindex: ! Rails.env.production?,
+      icon: [                    #favicon、apple用アイコンを指定する
+        { href: image_url('favicon.ico') },
+        # { href: image_url('icon.jpg'), rel: 'apple-touch-icon', sizes: '180x180', type: 'image/jpg' },
+      ],
+      og: {
+        site_name: 'バズダイアリー',
+        title: 'チャレンジミッション達成',
+        description: 'チャレンジミッションを達成したことを共有します', 
+        type: 'website',
+        url: request.original_url,
+        image: image_url('ogp.png'),
+        locale: 'ja_JP',
+      },
+      twitter: {
+        card: 'summary_large_image',
+        site: 'https://x.com/rgoto_51b',
+      }
+      # fb: {
+      #   app_id: '自身のfacebookのapplication ID'
+      # }
+    }
   end
 
   def assign_meta_tags(options = {})
@@ -18,7 +51,7 @@ module ApplicationHelper
     title = options[:title]
     description = options[:description]
     keywords = options[:keywords]
-    image = options[:image].presence || image_url('placeholder.png')
+    image = options[:image].presence || image_url('ogp.png')
     configs = {
       separator: '|',
       reverse: true,
