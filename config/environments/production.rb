83348a71f6908ac6a00a9d5ad0a6778856ff42b3
ryusuke-goto <ryusukeryusuke0401@gmail.com>
2024-10-10 23:46:46 +0900

    configをwwwなしにしてみる

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -97,5 +97,5 @@ Rails.application.configure do
   # ]
   # Skip DNS rebinding protection for the default health check endpoint.
   # config.host_authorization = { exclude: ->(request) { request.path == "/up" } }
-  # config.hosts << 'www.buzzdiaries.com'
+  config.hosts << 'buzzdiaries.com'
 end
