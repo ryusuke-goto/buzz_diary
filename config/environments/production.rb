90108aa741f693e91fb2f82a82fc5c167d91675a
ryusuke-goto <ryusukeryusuke0401@gmail.com>
2024-10-10 23:47:44 +0900

    configを*.buzzdiaries.comとbuzzdiaries.comの二つに。

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -98,4 +98,5 @@ Rails.application.configure do
   # Skip DNS rebinding protection for the default health check endpoint.
   # config.host_authorization = { exclude: ->(request) { request.path == "/up" } }
   config.hosts << 'buzzdiaries.com'
+  config.hosts << '*.buzzdiaries.com'
 end
