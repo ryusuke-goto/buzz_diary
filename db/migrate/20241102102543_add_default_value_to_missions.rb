3a0d5990b915dc3ad7ef558a1ebf5e30ece62baf
ryusuke-goto <151528192+ryusuke-goto@users.noreply.github.com>
2024-11-02 20:15:59 +0900

    Revert "rubocopのBulkChangeTableとNotNullColumnで指摘された部分を対応した上で、ymlファイルで無視する記述"

--- db/migrate/20241102102543_add_default_value_to_missions.rb
+++
@@ -1,8 +0,0 @@
-# frozen_string_literal: true
-
-class AddDefaultValueToMissions < ActiveRecord::Migration[7.1]
-  def change
-    change_column_default :daily_missions, :description, from: nil, to: 'ミッションの説明'
-    change_column_default :challenge_missions, :description, from: nil, to: 'ミッションの説明'
-  end
-end
