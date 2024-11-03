64f90cc9955ed994415b601b720eff71aab9d9ac
ryusuke-goto <ryusukeryusuke0401@gmail.com>
2024-11-03 23:46:54 +0900

    test:rubocopのUniqueness validationを対応

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_11_02_112850) do
+ActiveRecord::Schema[7.1].define(version: 2024_11_03_144501) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -61,6 +61,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_11_02_112850) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.date "diary_date", default: -> { "CURRENT_DATE" }, null: false
+    t.index ["diary_date", "user_id"], name: "index_diaries_on_diary_date_and_user_id", unique: true
     t.index ["user_id"], name: "index_diaries_on_user_id"
   end
 
@@ -70,6 +71,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_11_02_112850) do
     t.bigint "diary_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["diary_id", "user_id"], name: "index_likes_on_diary_id_and_user_id", unique: true
     t.index ["diary_id"], name: "index_likes_on_diary_id"
     t.index ["user_id"], name: "index_likes_on_user_id"
   end
